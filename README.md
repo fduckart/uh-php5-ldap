5e7f121f5351d827af736c5d48470baaf469ff33
Frank Duckart <duckart@computer.org>
2015-02-18 08:32:18 -1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,63 @@
+uh-php5-ldap
+===========
+
+LDAP PHP-5 Demonstration Program
+
+**Overview**
+
+The program demonstrates how to do a simple LDAP search
+of the University of Hawaii LDAP service. 
+
+Note: You must specify your special DN and its password.<br/>
+The example run of the program uses the 'filedrop' special DN,
+but the value of the password has been removed from the 
+checked-in code. The use of the special DN will also require 
+the ability to pass through the UH firewall.
+
+**Technology**
+
+The program was developed on Apple Mac OS X 10.10.2,
+using PHP version 5.5.14 and GNU bash, version 3.2.57.
+
+**Verify you have PHP installed**
+
+	$ php --version
+	PHP 5.5.14 (cli) (built: Sep  9 2014 19:09:25) 
+	Copyright (c) 1997-2014 The PHP Group
+	Zend Engine v2.5.0, Copyright (c) 1998-2014 Zend Technologies
+
+
+**Running the Program**
+
+Run the program from the command line: 
+
+    $ ./ldaprunner duckart
+	vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+          cn --> Frank R Duckart
+         uid --> duckart
+      uhuuid --> 17958670
+        mail --> duckart@hawaii.edu
+        mail --> frank.duckart@hawaii.edu
+    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+You can run the program with mutltiple UH usernames: </br>
+
+    $ ./ldaprunner duckart duckart
+	vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+          cn --> Frank R Duckart
+         uid --> duckart
+      uhuuid --> 17958670
+        mail --> duckart@hawaii.edu
+        mail --> frank.duckart@hawaii.edu
+    	................................
+          cn --> Frank R Duckart
+         uid --> duckart
+      uhuuid --> 17958670
+        mail --> duckart@hawaii.edu
+        mail --> frank.duckart@hawaii.edu
+	^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+**For More Information**
+
+Contact me via email at duckart@hawaii.edu
+
---
+++ ldaprunner.php
@@ -0,0 +1,73 @@
+<?php
+
+class Host 
+{
+    const HOST = 'ldaps://ldap.hawaii.edu';
+    const PORT = 636;
+
+    function host() {
+        return self::HOST;
+    }
+    
+    function port() {
+        return self::PORT;
+    }    
+}
+
+class LdapRunner
+{    
+    const BINDDN   = 'cn=filedrop,ou=Specials,dc=hawaii,dc=edu';
+    const PASSWORD = ''; // <- Your password goes here.
+    const SEARCHBASE = 'dc=hawaii,dc=edu';
+    
+    private $connection;
+    
+    function __construct() {
+        $h = new Host();
+        $conn = ldap_connect($h->host(), $h->port());
+                
+        ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
+        ldap_set_option($conn, LDAP_OPT_REFERRALS, 0);
+        ldap_set_option($conn, LDAP_OPT_TIMELIMIT, 5);            
+        ldap_set_option($conn, LDAP_OPT_NETWORK_TIMEOUT, 5); 
+        
+        $this->connection = $conn;        
+    }
+
+    function __destruct() {
+        if (isset($this->connection)) {
+            ldap_close($this->connection);            
+        }
+    }
+       
+    function search($uid) {        
+
+        $conn = $this->connection;
+        $bound = ldap_bind($conn, self::BINDDN, self::PASSWORD);
+
+        $attributes = array("cn", "uid", "uhUuid", "mail");
+        $filter = "(uid=$uid)";
+
+        $result = ldap_search($conn, self::SEARCHBASE, $filter, $attributes);
+        $info = ldap_get_entries($conn, $result);
+
+        if ($info["count"] > 0) {
+            for ($i = 0; $i < $info["count"]; $i++) {
+                for ($j = 0; $j < $info[$i]["count"]; $j++){
+                    $data = $info[$i][$j];
+                    for ($k = 0; $k < $info[$i][$data]["count"]; $k++) {
+                        printf("%12s --> %s\n", $data, $info[$i][$data][$k]);
+                    }
+                }
+            }
+        }
+    }    
+}
+
+// Main program.
+$runner = new LdapRunner();
+for ($i = 1; $i < count($argv); $i++) {
+	$runner->search($argv[$i]);	
+}
+
+?>
